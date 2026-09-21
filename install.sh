b2c2b14e5c6be265d7c98f3796e8d52e9513db3f
VitãoTub <160746536+vitaotub@users.noreply.github.com>
2026-09-21 15:44:10 -0300

    Update version to 1.0.0-rc.3 in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # ============================================================
 # Fedora Only Fans (FOF) - Script de Instalação
-# Versão: 1.0.0-rc.2
+# Versão: 1.0.0-rc.3
 # ============================================================
 
 set -e
@@ -13,7 +13,7 @@ BLUE='\033[0;34m'
 CYAN='\033[0;36m'
 NC='\033[0m'
 
-VERSION="1.0.0-rc.2"
+VERSION="1.0.0-rc.3"
 INSTALL_DIR="$HOME/.local/share/fedora-only-fans"
 BIN_DIR="$HOME/.local/bin"
 
