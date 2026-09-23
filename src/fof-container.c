cf9bbd505ebb01615a6382702fc730e4283b74f0
VitãoTub <160746536+vitaotub@users.noreply.github.com>
2026-09-22 23:07:01 -0300

    Add files via upload

--- src/fof-container.c
+++ src/fof-container.c
@@ -1,6 +1,5 @@
 // ============================================================
 // Fedora Only Fans (FOF) - Container WebKitGTK
-// Versão: 1.0.0-rc.3
 // ============================================================
 
 #include <gtk/gtk.h>
@@ -308,7 +307,7 @@ window_height = atoi(argv[++i]);
 } else if (strcmp(argv[i], "--debug") == 0) {
 debug_mode = 1;
 } else if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
-printf("🐧 Fedora Only Fans - Container WebKitGTK v1.0.0-rc.3\n");
+printf("🐧 Fedora Only Fans - Container WebKitGTK v1.0.0-09222026\n");
 printf("\nUso: %s [opções]\n", argv[0]);
 printf("\nOpções:\n");
 printf(" --url URL URL do servidor (padrão: http://localhost:3000)\n");
